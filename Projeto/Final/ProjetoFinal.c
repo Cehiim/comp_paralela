339778bef584d9e4131c684fd669b1ea46bc36d6
Cesar Hideki Imai <116912049+Cehiim@users.noreply.github.com>
2024-05-28 16:52:58 -0300

    Update ProjetoFinal.c

--- Projeto/Final/ProjetoFinal.c
+++ Projeto/Final/ProjetoFinal.c
@@ -1,33 +1,32 @@
 #include <stdio.h> // Biblioteca de input/output
 #include <stdlib.h> // Biblioteca de alocação de memória
 #include <pthread.h> // Biblioteca de manipulação de threads
-#include <mpfr.h> // Biblioteca de cálculo de precisão
+#include <mpfr.h> // Biblioteca de cálculo de precisão arbitrária
 
 #define N_CASA 100000
-
-mpfr_t result;
 int numdeexec = 1000;
-int numporthread = 0;
 int numthreads = 16;
+mpfr_t result;
+int numporthread = numdeexec/numthreads;
 
 
 void fatorial (int n, mpfr_t fat_result) { // Função para calcular o fatorial de um número 'n'
-  mpfr_set_ui(fat_result, 1.0, MPFR_RNDU); // Configura o valor de "fat_result" para 1.0, "MPFR_RNDU" é uma constante que indica o modo de arredondamento para cima
+  mpfr_set_ui(fat_result, 1.0, MPFR_RNDU); // Define o valor de "fat_result" para 1.0, "MPFR_RNDU" é uma constante que indica o modo de arredondamento para cima
   for (int i = 1; i < n+1; i ++){
     mpfr_mul_ui(fat_result, fat_result, i, MPFR_RNDU); // Dentro do loop, "fat_result" é multiplicado pelo valor de 'i'
   }
 }
 
-void *threadexec(void* args) {
+void *threadexec(void* args) { // Função de execução para cada thread
 int numthread = (int)args;
 mpfr_t temp, fat, div, um;
-mpfr_init2(temp, N_CASA);
+mpfr_init2(temp, N_CASA); // Define a precisão especificada para cada variável (número de casas decimais após o ponto)
 mpfr_init2(fat, N_CASA);
 mpfr_init2(div, N_CASA);
 mpfr_init2(um, N_CASA);
 int aux;
-mpfr_set_d(temp, 0.0, MPFR_RNDU);
-mpfr_set_d(um, 1.0, MPFR_RNDU);
+mpfr_set_d(temp, 0.0, MPFR_RNDU); // Define o valor de "temp" para 0.0
+mpfr_set_d(um, 1.0, MPFR_RNDU); // Define o valor de "um" para 1.0
 for (int i = 0; i < numporthread; i ++) {
 aux = ((i*numthreads) + numthread);
 fatorial(aux, fat);
@@ -44,9 +43,9 @@ return NULL;
 
 int main() {
 pthread_t threads[numthreads];
-numporthread = numdeexec/numthreads;
 mpfr_init2(result, N_CASA);
 mpfr_set_d(result, 0.0, MPFR_RNDU);
+  
 for (int i = 0; i < numthreads; i++) {
 pthread_create(&threads[i], NULL, threadexec, (void*)i);
 }
